4d70b8326666dae0a6ee658d24e1240381d63292
jayasankar-shyam <jayasankar06shyam@gmail.com>
2022-06-25 18:35:44 +0530

    brightness

--- brightness.sh
+++ brightness.sh
@@ -1,3 +1,12 @@
-echo "Enter level : "
-read n
-rogauracore brightness $n
+n=0
+while(n==1||n==2||n==3||n==0)
+do
+	echo "Enter level : " #(x to exit) 
+	read n
+	#if (( $n = "x"|| $n = "X" )) 
+    	#then break 
+	#else 
+    	rogauracore brightness $n 
+	#fi
+done
+
